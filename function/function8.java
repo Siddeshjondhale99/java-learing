bcd844f3196837efc7448ee7b9ca1885ace69a47
Siddeshjondhale99 <siddeshjondhale99@gmail.com>
2025-02-09 14:41:00 +0530

    these is new change feb 8

---
+++ function/function8.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class function8 {
+   public static  boolean isprime( int n)
+   {
+      boolean isprime = true;
+      for(int i=2;i<=Math.sqrt(n);i++)
+      {
+         if(n % i == 0)
+         {
+            isprime = false; 
+         }
+      }
+      return isprime ;
+   }
+   public static void primerange(int a)
+   {
+      for(int i = 2 ; i <= a; i++)
+      {
+         if(isprime(a)
+         {
+            System.out.print(i + " ");
+         }
+      }
+      System.out.println();
+
+   }
+   public static void main(String args[])
+   {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      System.out.print(primerang)
+      
+
+   }
+   
+}
