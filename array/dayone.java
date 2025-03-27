2101907d55db3d5421aadb164bd3e33d91cb7bb0
Siddeshjondhale99 <siddeshjondhale99@gmail.com>
2025-03-27 19:26:03 +0530

    this

---
+++ array/dayone.java
@@ -0,0 +1,15 @@
+
+
+public class dayone 
+{
+   public static void main(String args[])
+   {
+      int marks[] = {1,2,3,4,5};
+      String maha[] = {"asha" , "omya" , "kavya"};
+      for(int i=0;i <= marks.length ; i++){
+         System.out.println(marks);
+      }
+      
+   }
+   
+}
